386c45b95578d4987160d4e26d970ef0ddb36820
ronneyhuang <ronneyhuang@wesure.cn>
2022-07-04 14:54:37 +0800

    chore: add usePagination interface

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,8 +1,16 @@
 import { ref } from "vue";
+import type { Ref } from 'vue'
 
-export default function usePagination() {
-  const pageSize = ref(10);
-  const pageNum = ref(1);
+export interface PaginationRes {
+  pageNum: Ref<number>,
+  pageSize: Ref<number>,
+  onPageUpdate: (e: number) => void,
+  onPageSizeUpdate: (e: number) => void
+}
+
+export default function usePagination(defaultNum:number = 1, defaultSize:number = 10,): PaginationRes {
+  const pageSize = ref(defaultSize);
+  const pageNum = ref(defaultNum);
 
   const onPageUpdate = (e: number) => {
     pageNum.value = e;
